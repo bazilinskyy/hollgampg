db4c9e47cdf0c4a9a9f1edc2c72c3fe4eb0a9819
Pavlo Bazilinskyy <hollgam@gmail.com>
2011-06-18 16:50:40 +0300

    Django test project JUnit test project

---
+++ JUnit/test1/src/test1/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author Pavlo Bazilinskyy
+ */
+public class Main {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        StringUtilsTest test1 = new StringUtilsJUnit3Test();
+//        
+//        test1.testIsEmpty1();
+    }
+    
+}
---
+++ JUnit/test1/src/test1/StringUtilsJUnit3Test.java
@@ -0,0 +1,20 @@
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package test1;
+//
+//import junit.framework.TestCase;
+//
+///**
+// *
+// * @author hollgam
+// */
+//public class StringUtilsJUnit3Test extends TestCase{
+//
+//    public void testIsEmpty1() {
+//        boolean actual = StringUtils.isEmpty(null);
+//        assertTrue(actual);
+//        
+//    }
+//}
